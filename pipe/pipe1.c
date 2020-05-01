9fa453cfe4b59749c96036afe5d3f7fcf2b8f762
wangzhengquan <wangzhengquan85@126.com>
2020-05-01 21:17:19 +0800

    update

---
+++ pipe/pipe1.c
@@ -0,0 +1,37 @@
+#include "apue.h"
+
+  int
+main(void)
+{
+  int   n, i;
+  int   fd[2],fd2[2];
+  pid_t pid;
+  char  line[MAXLINE];
+
+  if (pipe(fd) < 0)
+    err_sys("pipe error");
+  if (pipe(fd2) < 0)
+    err_sys("pipe error");
+
+  if ((pid = fork()) < 0) {
+    err_sys("fork error");
+  } else if (pid > 0) {   /* parent */
+    close(fd[0]);
+    close(fd2[1]);
+    while((n = read(STDIN_FILENO, line, MAXLINE)) > 0) {
+      write(fd[1], line, n);
+      n = read(fd2[0], line, MAXLINE);
+      write(STDOUT_FILENO, line, n);
+    }
+  } else {          /* child */
+    close(fd[1]);
+    close(fd2[0]);
+    while((n = read(fd[0], line, MAXLINE)) > 0) {
+      for(i = 0; i<n; i++) {
+        line[i] = toupper(line[i]);
+      }
+      write(fd2[1], line, n);
+    }
+  }
+  exit(0);
+}
---
+++ pipe/test.c
@@ -0,0 +1,14 @@
+#include "apue.h"
+
+int main(void)
+{
+  int fd;
+  char line[100];
+  int n;
+  fd = open("./hello.txt", O_RDONLY);
+  sleep(10);
+  while((n = read(fd, line, 100)) > 0) {
+    write(1, line, n);
+  } 
+  exit(0);
+}
