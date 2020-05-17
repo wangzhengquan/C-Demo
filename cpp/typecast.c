b2988e6d9651bd90ef1f2f21db2f45c8cf8fd2e2
wangzhengquan <wangzhengquan85@126.com>
2020-05-17 13:10:15 +0800

    udpate

--- cpp/typecast.c
+++ cpp/typecast.c
@@ -1,23 +1,50 @@
 #include <iostream>
 using std::ostream;
 using std::istream;
-
+using namespace std;
+/**
+ * dynamic_cast 子类转换为父类。 static_cast 同类之间相互转换。
+ */
 class Animal {
-private:
+protected:
 	string name;
-
 public:
-	virtual run();
 
+	Animal() {name="Animal";}
+	string getName(){ return name;};
+	virtual void run() const{};
 };
 
-class Cat :: Animal {
-	virtual run();
+class Cat : public Animal {
+public:
+	Cat() {name="Cat";}
+	virtual void run() const{};
+	 
+};
+
+class Dog : public Animal {
+public:
+	Dog() {name="Dog";}
+	virtual void run() const{};
 };
 
 class Plant {
-private:
-	string name;
 public:
-	virtual 
+	Plant() {}
+};
+
+class Tree : public Plant {
+public:
+	Tree() {}
+};
+
+class Flower : public Plant {
+public:
+	Flower() {}
+};
+
+
+
+int main() {
+	
 }
