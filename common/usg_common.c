e9f0da1e03730d696c003249cb720bcda362f89c
wangzhengquan <wangzhengquan85@126.com>
2020-06-01 22:11:01 +0800

    udpate

--- common/usg_common.c
+++ common/usg_common.c
@@ -68,7 +68,9 @@ static void err_doit(int error, const char *fmt, va_list ap)
 	char	buf[MAXLINE];
 
 	vsnprintf(buf, MAXLINE-1, fmt, ap);
-	snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s", strerror(error));
+	if (error != 0) {
+		snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s", strerror(error));	
+	}
 	strcat(buf, "\n");
 	fflush(stdout);		/* in case stdout and stderr are the same */
 	fputs(buf, stderr);
